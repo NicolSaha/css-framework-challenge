4dfa8d8b804b35e558936d038c8e7d9fa655e2eb
“[Nicol <[nicol.saha@gmail.com]>
2020-07-03 20:07:04 +0200

    purge css fix

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ module.exports = {
     require("tailwindcss"),
     require("autoprefixer"),
     require("@fullhuman/postcss-purgecss")({
-      content: ["./index.html", "./otherfiles?"],
+      content: ["./index.html", "./page2.html"],
       defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
     }),
   ],
