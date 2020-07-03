1f32fcffc80cee1aaa08107c9ee2c49ba1a4e797
“[Nicol <[nicol.saha@gmail.com]>
2020-07-03 21:13:57 +0200

    purge css fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-color": "#E7EFF5",
+      },
+      height: {
+        "h-nici-395": '395px',
+        "h-nici-100": '100px',
+      },
+      width: {
+        "w-nici-280": '280px',
+        "w-nici-100": '100px',
+      },
+    },
   },
   variants: {},
   plugins: [],
-}
+};
